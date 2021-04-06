8115fa5d07a5f3b1c904b5f37ec150fbce330cba
Brice Hoareau <bricesh@yahoo.com>
2021-04-06 13:29:35 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import app_constants as aconst
+#import app_helper as ahelp
+from shapash.utils.load_smartpredictor import load_smartpredictor
+import plotly.express as px
+
+def roundup(x):
+	return x if x % 1000 == 0 else x + 1000 - x % 1000
+
+st.title('House Price Predictor')
+#import model
+predictor_load = load_smartpredictor('./model/predictor.pkl')
+
+#import necessary files
+osm_count = pd.read_csv('./data/prod_osm_count.csv')
+population = pd.read_csv('./data/prod_population.csv')
+master = pd.read_csv('./data/prod_master.csv')
+geo = pd.read_csv('./data/prod_geo.csv')
+
+#create UI objects
+st.sidebar.markdown('## Set characteristics:')
+geo_plz = st.sidebar.number_input('Postcode', format='%i', value=81549, max_value=99998)
+obj_data = {}
+#obj_data['geo_plz'] = geo_plz
+for list_name, list_values in aconst.input_lists.items():
+	list_values.sort()
+	obj_data[list_name] = st.sidebar.selectbox(aconst.feature_dict[list_name], list_values, key=list_name)
+for slider_name, slider_values in aconst.input_sliders.items():
+	obj_data[slider_name] = st.sidebar.slider(aconst.feature_dict[slider_name], key=slider_name, min_value=slider_values[0], max_value=slider_values[1], value=slider_values[2])
+for check_name, check_values in aconst.input_checks.items():
+	obj_data[check_name] = st.sidebar.checkbox(aconst.feature_dict[check_name], key=check_name)
+
+obj_data['obj_energyEfficiencyClass'] = aconst.energy_rating_mapping[obj_data['obj_energyEfficiencyClass']]
+obj_data['obj_condition'] = aconst.condition_mapping[obj_data['obj_condition']]
+obj_data['obj_interiorQual'] = aconst.interior_condition_mapping[obj_data['obj_interiorQual']]
+#obj_data['obj_heatingType'] = aconst.heating_mapping[obj_data['obj_heatingType']]
+	
+macro_data = pd.merge(pd.merge(pd.merge(osm_count, master, on="geo_plz"), geo, on="geo_plz"), population, on="geo_plz")
+macro_data['density'] = macro_data['einwohner'] / (macro_data['area'] * 1000)
+macro_data = macro_data[macro_data['geo_plz'] == geo_plz][aconst.macro_cols].to_dict(orient='records')[0]
+data_to_predict = {**obj_data, **macro_data}
+predictor_load.add_input(x=data_to_predict)
+
+detailed_contributions = predictor_load.detail_contributions()
+price_pred = roundup(int(detailed_contributions.iloc[0,0] * aconst.HOUSE_PRICE_INDEX))
+min_price_pred = price_pred - 85000
+max_price_pred = price_pred + 85000
+
+
+cols = st.beta_columns([1,1,1])
+with cols[0]:
+	st.write('_€'+str(min_price_pred)+'_')
+with cols[1]:
+	st.write('__€'+str(price_pred)+'__')
+with cols[2]:
+	st.write('_€'+str(max_price_pred)+'_')
+
+detailed_contributions = detailed_contributions[list(data_to_predict.keys())].T.sort_values(by=0)
+detailed_contributions.columns = ['Feature']
+detailed_contributions.rename(aconst.feature_dict, axis='rows', inplace = True)
+detailed_contributions['colour'] = detailed_contributions['Feature'].apply(lambda x: ('pos' if x > 0 else 'neg'))
+fig = px.bar(detailed_contributions,
+	x='Feature',
+	orientation='h',
+	color='colour',
+	labels=dict(Feature = 'Contribution', index=''),
+	width=500,
+	height=700,
+	#hover_name='Feature',
+	#hover_data={'colour': True, 'Feature': False},
+	color_discrete_sequence=px.colors.qualitative.Set2[1:])
+fig.layout.update(showlegend=False, plot_bgcolor='rgb(252,252,252)')
+fig.update_xaxes(showgrid=True, gridwidth=2, gridcolor='rgb(200,200,200)')
+fig.update_traces(
+    #hovertext='{x.index.values}',
+    hovertemplate='€%{x:.0f}')
+st.plotly_chart(fig, use_container_width=True)
---
+++ app_constants.py
@@ -0,0 +1,114 @@
+feature_dict = {
+	'obj_heatingType': 'Heating Type',
+	'obj_condition': 'Building Condition',
+	'obj_buildingType': 'Building Type',
+	'obj_energyEfficiencyClass': 'Energy Efficiency Class',
+	'obj_interiorQual': 'Interior Quality',
+	'obj_yearConstructed': 'Year Constructed',
+	'obj_cellar': 'Has Cellar',
+	'obj_livingSpace': 'Living Area',
+	'obj_noRooms': 'Number of Rooms',
+	'obj_noParkSpaces': 'Number of Parking Spaces',
+	'obj_lotArea': 'Plot Area',
+	'obj_courtage': 'Courtage',
+	'bus_count': 'Number of Bus Stops',
+	'food_count': 'Number of Restaurants',
+	'gdp_per_hab': 'GDP per Habitant',
+	'doctor_count': 'Number of Doctors',
+	'child_pop': 'Child Age Population',
+	'working_pop': 'Working Age Population',
+	'retired_pop': 'Retirement Age Population',
+	'residencial_building_per_qkm': 'Residential Buildings/km2',
+	'percent_employed': 'Percent of Population Employed',
+	'wage_per_per': 'Wage per Habitant',
+	'percent_floor_area_traffic': 'Percent Traffic Area',
+	'percent_floor_area_vegetation': 'Percent Vegetation Area',
+	'percent_floor_use_residential': 'Percent Residential Area',
+	'percent_floor_use_industry_commerce': 'Percent Industry/Commerce Area',
+	'percent_floor_use_leisure': 'Percent Leisure Area',
+	'density': 'Population Density',
+	'ppl_per_building': 'Habitants per Building',
+	'highrise_factor': 'High Rise Factor',
+	'distance': 'Distance to Closest Large City',
+	'new_apartment_permit_per_hab': 'New Aprt Permits per Habitant'}
+
+input_lists = {
+	'obj_buildingType': ['single_family_house', 'semidetached_house', 'multi_family_house',
+		'other_real_estate', 'mid_terrace_house', 'farmhouse', 'villa',
+		'end_terrace_house', 'special_real_estate', 'bungalow', 'castle_manor_house'],
+	'obj_heatingType': ['heat_pump', 'gas_heating', 'central_heating', 'floor_heating',
+		'electric_heating', 'stove_heating', 'oil_heating',
+		'self_contained_central_heating', 'night_storage_heater', 'district_heating',
+		'combined_heat_and_power_plant', 'wood_pellet_heating', 'solar_heating'],
+	'obj_condition': ['first_time_use', 'well_kept', 'mint_condition', 'need_of_renovation',
+		'refurbished', 'fully_renovated', 'modernized', 'first_time_use_after_refurbishment',
+		'ripe_for_demolition'],
+	'obj_interiorQual': ['sophisticated', 'normal', 'simple', 'luxury'],
+	'obj_energyEfficiencyClass': ['A_PLUS', 'D', 'H', 'C', 'E', 'F', 'B', 'A', 'G'],	
+}
+
+input_sliders = {
+	'obj_yearConstructed': (1960, 2020, 1980),
+	'obj_livingSpace': (18, 300, 100),
+	'obj_lotArea': (0, 1000, 500),
+	'obj_noRooms': (1, 10, 4),
+	'obj_noParkSpaces': (0, 10, 2),	
+}
+
+input_checks = {
+	'obj_courtage': (False, True),
+	'obj_cellar': (False, True)
+}
+
+macro_cols = ['bus_count', 'food_count', 'gdp_per_hab',
+	'doctor_count', 'child_pop', 'working_pop', 'retired_pop', 'residencial_building_per_qkm',
+	'percent_employed', 'wage_per_per', 'percent_floor_area_traffic',
+	'percent_floor_area_vegetation', 'percent_floor_use_residential',
+	'percent_floor_use_industry_commerce', 'percent_floor_use_leisure', 'density',
+	'ppl_per_building', 'highrise_factor', 'distance', 'new_apartment_permit_per_hab']
+
+HOUSE_PRICE_INDEX = 1.1
+
+energy_rating_mapping = {'A_PLUS': 9,
+                         'A': 8,
+                         'B': 7,
+                         'C': 6,
+                         'D': 5,
+                         'E': 4,
+                         'F': 3,
+                         'G': 2,
+                         'H': 1}
+
+condition_mapping = {'first_time_use': 70,
+                     'mint_condition': 55,
+                     'first_time_use_after_refurbishment': 45,
+                     'modernized': 40,
+                     'well_kept': 40,
+                     'fully_renovated': 35,
+                     'refurbished': 35,
+                     'need_of_renovation': 20,
+                     'ripe_for_demolition': 0,
+                     }
+
+interior_condition_mapping = {'luxury': 6,
+                              'sophisticated': 4,
+                              'normal': 2,
+                              'simple': 1}
+
+heating_mapping = {'floor_heating': 55,
+                   'district_heating': 52,
+                   'combined_heat_and_power_plant': 51,
+                   'solar_heating': 49,
+                   'heat_pump': 48,
+                   'wood_pellet_heating': 46,
+                   'central_heating': 44,
+                   'gas_heating': 43,
+                   'oil_heating': 43,
+                   'self_contained_central_heating': 43,
+                   'electric_heating': 32,
+                   'night_storage_heater': 31,
+                   'stove_heating': 0,
+                  }
+				  
+if __name__ == '__main__':
+    print("should be used as import")
